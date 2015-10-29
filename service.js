88961c04cada9acdf43fd8807f89b6a901c57e79
Tim Berry <timothyaberry@gmail.com>
2015-10-29 12:14:19 -0600

    Completed project

--- service.js
+++ service.js
@@ -1,12 +1,25 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+
+  	var deferred = $q.defer();
+
+    $http({
         method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
-  }
+        url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response){
+    	var parsedResponse = response.data.data;
+
+    	parsedResponse.forEach(function(user){
+    		user.first_name = 'Ralf';
+    	});
+
+    	deferred.resolve(parsedResponse);
+    });
+
+    return deferred.promise;
+  };
 
 });
